c3c733f2a3989805dbc229754ef6c3d0b9b075ce
ajay <ajaysunarthi@iitj.ac.in>
2016-06-12 23:47:12 +0530

    server added

---
+++ mapapp/server.js
@@ -0,0 +1,25 @@
+
+var express         = require('express');
+var mongoose        = require('mongoose');
+var port            = process.env.PORT || 3000;
+var morgan          = require('morgan');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var app             = express();
+
+
+mongoose.connect("mongodb://localhost/mapapp");
+
+
+app.use(express.static(__dirname + '/public'));                
+app.use('/node_modules',  express.static(__dirname + '/node_modules')); 
+app.use(morgan('dev'));                                         // log with Morgan
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+
+app.listen(port);
+console.log('App listening on port ' + port);
